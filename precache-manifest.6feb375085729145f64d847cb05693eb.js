a36d3f0c67f86585db9a56eecd4f6d6dca71e5d7
aivis <aiviskle@gmail.com>
2020-05-27 12:59:40 +0300

    Updates

---
+++ precache-manifest.6feb375085729145f64d847cb05693eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f370d1114c776c7a3a84cbadb905471",
+    "url": "/slider/index.html"
+  },
+  {
+    "revision": "93040b3ce373d2c55b94",
+    "url": "/slider/static/css/main.78bcb3b8.chunk.css"
+  },
+  {
+    "revision": "99cfa0f1834acfb54f1b",
+    "url": "/slider/static/js/2.d4deb131.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/slider/static/js/2.d4deb131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93040b3ce373d2c55b94",
+    "url": "/slider/static/js/main.40778adf.chunk.js"
+  },
+  {
+    "revision": "9b8d4e560076c5ec6cf5",
+    "url": "/slider/static/js/runtime-main.b1310a78.js"
+  },
+  {
+    "revision": "6773c5a24fae57d2add26f0d5c2ae725",
+    "url": "/slider/static/media/image1.6773c5a2.jpg"
+  },
+  {
+    "revision": "3de6d863469f03fe65bb0f84761a3783",
+    "url": "/slider/static/media/image2.3de6d863.jpg"
+  }
+]);
